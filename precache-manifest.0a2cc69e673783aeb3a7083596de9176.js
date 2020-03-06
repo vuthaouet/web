01e095f762daa32b964f99da66d5a351907982d0
unknown <vuthao.ak56ht@gmail.com>
2020-03-06 17:13:42 +0700

    Updates

---
+++ precache-manifest.0a2cc69e673783aeb3a7083596de9176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbd75f871cdfe62f4ba745d38fd4864",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "ca2d132a4c0a78ab69f6",
+    "url": "/web/static/css/main.3f13f529.chunk.css"
+  },
+  {
+    "revision": "1e9615e65bc5638713f1",
+    "url": "/web/static/js/2.d2d46fe9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/web/static/js/2.d2d46fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2d132a4c0a78ab69f6",
+    "url": "/web/static/js/main.15b7edfe.chunk.js"
+  },
+  {
+    "revision": "6b633183f0b0ab00345c",
+    "url": "/web/static/js/runtime-main.648a5511.js"
+  }
+]);
